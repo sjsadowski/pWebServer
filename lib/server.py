f7d1b05ef14e4bc3a323aa70d959fe6f4518132e
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-06-22 07:31:27 -0500

    Initial Commit

---
+++ lib/server.py
@@ -0,0 +1,20 @@
+import sys
+
+# Conditional import
+if sys.version.find('MicroPython'):
+    import uasyncio as asyncio # type: ignore
+else:
+    import asyncio
+
+class Server:
+
+    routes: list = []
+
+    def __init__(self) -> None:
+        pass
+
+    async def add_route(self, method: str, path: str, fn) -> None:
+        self.routes.append((method, path, fn))
+
+    async def serve(self):
+        pass
