73c9e6736e1f250df7d5d63466e82301fd313d07
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2023-06-30 08:04:00 -0500

    FEAT: add/expand parsing functions to request

--- src/server.py
+++ src/server.py
@@ -11,6 +11,7 @@ else:
 class Request:
 
     # headers is a list of tuples with the header name and its value
+    request: tuple = ()
     headers: list = []
     query_params: list = []
     body: str = ''
@@ -23,25 +24,35 @@ class Request:
     # TODO: Process form data
     # TODO: Store body
     async def parse(self, reader):
-        logging.debug("parsing request")
-        request_line = await reader.readline()
-        logging.debug("Request:", request_line)
+        '''Parse an html text request
+        '''
+        await self._parse_request(reader)
+        await self._parse_headers(reader)
+        await self._parse_body(reader)
 
 
+    async def _parse_headers(self, reader) -> None:
+        '''(Private) parse request headers
+        '''
         # The below reads the incoming request until it hits an empty line
-
         for header_line in reader.readline():
             if header_line == b"\r\n":
                 break
             key, value = header_line.split(':')
             self.headers.append((key, value))
 
-        request = str(request_line)
 
-    async def _parse_headers():
-        pass
+    async def _parse_request(self, reader) -> None:
+        '''(private) parse request line and determine method, path, and query params
+        '''
+        logging.debug("parsing request")
+        request_line = await reader.readline()
+        logging.debug("Request:", request_line)
 
-    async def _parse_query():
+
+    async def _parse_body(self, reader):
+        '''(private) parse body
+        '''
         pass
 
 class Response:
@@ -73,8 +84,8 @@ class Server:
 
 
         writer.write('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-        writer.write(response)
-
+        # writer.write(response)
+        writer.write('')
         await writer.drain()
         await writer.wait_closed()
         print("Client disconnected")
