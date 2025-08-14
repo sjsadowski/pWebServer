b55b3264d26862cfd4c465db7582be27d2222933
Stephen Sadowski <stephen.sadowski@sjsadowski.com>
2025-08-14 07:04:25 -0500

    TASK: add cpython example

---
+++ examples/cpython-basic.py
@@ -0,0 +1,8 @@
+from pWebServer import Server
+import asyncio
+
+sv = Server()
+sv.add_default_route() # simply adds a route at / that returns 200 OK
+loop = asyncio.get_event_loop()
+loop.create_task(sv.start()) # Start serving for 0.0.0.0 (all ipv4 ips) on port 80
+loop.run_forever()
